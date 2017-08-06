2edcc80a1f5dbe1ccf3a1c1071f8c38170654468
robeerob <rob@rob.ee>
2017-08-06 16:27:18 +0200

    Add additional handlers and refactor

--- main.go
+++ main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-  "net/http"
-  r "github.com/dancannon/gorethink"
-  "log"
+	r "github.com/dancannon/gorethink"
+	"log"
+	"net/http"
 )
 
-type Channel struct {
-  Id    string  `json:"id" gorethink:"id,omitempty"`
-  Name  string  `json:"name" gorethink:"name"`
-}
-
-type User struct {
-  Id string `gorethink:"id,omitempty"`
-  Name string `gorethink:"name"`
-}
-
 func main() {
-  session, err := r.Connect(r.ConnectOpts{
-    Address: "localhost:28015",
-    Database: "rtsupport",
-  })
-  if err != nil {
-    log.Panic(err.Error())
-  }
-  router := NewRouter(session)
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "rtsupport",
+	})
 
-  router.Handle("channel add", addChannel)
-  router.Handle("channel subscribe", subscribeChannel)
-  router.Handle("channel unsubscribe", unsubscribeChannel)
-  http.Handle("/", router)
-  http.ListenAndServe(":4000", nil)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	router := NewRouter(session)
+
+	router.Handle("channel add", addChannel)
+	router.Handle("channel subscribe", subscribeChannel)
+	router.Handle("channel unsubscribe", unsubscribeChannel)
+
+	router.Handle("user edit", editUser)
+	router.Handle("user subscribe", subscribeUser)
+	router.Handle("user unsubscribe", unsubscribeUser)
+
+	router.Handle("message add", addChannelMessage)
+	router.Handle("message subscribe", subscribeChannelMessage)
+	router.Handle("message unsubscribe", unsubscribeChannelMessage)
+	http.Handle("/", router)
+	http.ListenAndServe(":4000", nil)
 }
