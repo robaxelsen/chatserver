4eca984e0b3e78b515719392c1aa25fb51027859
robeerob <rob@rob.ee>
2017-08-06 15:43:11 +0200

    Expand and refactor channel subscription handling

--- client.go
+++ client.go
@@ -13,10 +13,17 @@ type Message struct {
 }
 
 type Client struct {
-  send        chan Message
-  socket      *websocket.Conn
-  findHandler FindHandler
-  session     *r.Session
+  send          chan Message
+  socket        *websocket.Conn
+  findHandler   FindHandler
+  session       *r.Session
+  stopChannels  map[int]chan bool
+}
+
+func (c *Client) NewStopChannel(stopKey int) chan bool {
+  stop := make(chan bool)
+  c.stopChannels[stopKey] = stop
+  return stop
 }
 
 func (client *Client) Read() {
@@ -41,11 +48,19 @@ func (client *Client) Write() {
   client.socket.Close()
 }
 
+func (c *Client) Close() {
+  for _, ch := range c.stopChannels {
+    ch <- true
+  }
+  close(c.send)
+}
+
 func NewClient(socket *websocket.Conn, findHandler FindHandler, session *r.Session) *Client {
   return &Client {
     send: make(chan Message),
     socket: socket,
     findHandler: findHandler,
     session: session,
+    stopChannels: make(map[int]chan bool),
   }
 }
